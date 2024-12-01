ff6871868d7d08c21b4c51ba0d5db1e6dd5d8240
emin <e.kutukcu@outlook.com>
2024-12-01 15:19:07 +0000

    Day 1 solutions

---
+++ day01.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "util.h"
+
+
+int main()
+{
+    FILE *input_file = fopen("input01.txt", "r");
+    if (input_file == NULL)
+    {
+        perror("Error: could not open file.");
+        return 1;
+    }
+
+    int size = 16;
+    int *list1 = (int *)calloc(size, sizeof(int));
+    int *list2 = (int *)calloc(size, sizeof(int));
+
+    int read_count = 0;
+    while (1)
+    {
+        int number1, number2;
+        int matched_count = fscanf(input_file, "%d   %d\n", &number1, &number2);
+        if (matched_count != 2)
+        {
+            printf("Could not read numbers from file, matched %d.", matched_count);
+        }
+        else
+        {
+            list1[read_count] = number1;
+            list2[read_count] = number2;
+            read_count++;
+
+            // double the size of the arrays
+            if (read_count == size)
+            {
+                printf("Re-allocating array from %d to %d\n", size, size * 2);
+                size *= 2;
+                list1 = (int *)realloc(list1, sizeof(int)*size);
+                list2 = (int *)realloc(list2, sizeof(int)*size);
+            }
+        }
+        if (feof(input_file))
+        {
+            break;
+        }
+    }
+
+    printf("Read %d lines.\n", read_count);
+    qsort(list1, read_count, sizeof(int), compare_ints);
+    qsort(list2, read_count, sizeof(int), compare_ints);
+    int total = 0;
+    for (int i = 0; i < read_count; i++)
+    {
+        int diff = abs(list1[i] - list2[i]);
+        total += diff;
+        printf("%d %d ---> %d, total=%d\n", list1[i], list2[i], diff, total);
+    }
+
+    fclose(input_file);
+    printf("Total difference: %d\n", total);
+
+    struct hash_table *list2_dict = hash_table_create(read_count);
+    for (int i = 0; i < read_count; i++)
+    {
+        struct hash_table_entry *found = hash_table_lookup(list2_dict, list2[i]);
+        if (found == NULL)
+        {
+            hash_table_insert(list2_dict, list2[i], 1);
+        }
+        else
+        {
+            hash_table_insert(list2_dict, list2[i], found->value + 1);
+        }
+    }
+
+    int similarity_score = 0;
+    for (int i = 0; i < read_count; i++)
+    {
+        int value = list1[i];
+        struct hash_table_entry *found = hash_table_lookup(list2_dict, value);
+        if (found != NULL)
+        {
+            similarity_score += value * found->value;
+        }
+    }
+    printf("Similarity score: %d", similarity_score);
+}
---
+++ util.h
@@ -0,0 +1,83 @@
+
+#include <string.h>
+#include <math.h>
+#include <stdbool.h>
+
+int compare_ints(const void *a, const void *b)
+{
+    int value_a = *(int *)a;
+    int value_b = *(int *)b;
+    return value_a - value_b;
+}
+
+const unsigned int ALPHA = 2654435769; // 2^32 (-1 + sqrt(5)) / 2
+int hash(int value, int hash_table_size)
+{
+    // Knuth multiplicative hash 
+    unsigned int unsigned_value = (unsigned int)value;
+    return (value*ALPHA)>>(32-hash_table_size);
+}
+
+struct hash_table_entry
+{
+    struct hash_table_entry *next;
+    int key;
+    int value;
+};
+
+struct hash_table
+{
+    struct hash_table_entry **entries;
+    int size;
+    int key_size;
+};
+
+struct hash_table *hash_table_create(int min_capacity)
+{
+    struct hash_table *dict = malloc(sizeof(struct hash_table));
+    dict->key_size = (int)floor(log2(min_capacity))+1;
+    dict->size = (int)pow(2,dict->key_size);;
+    dict->entries = (struct hash_table_entry **)calloc(dict->size, sizeof(struct hash_table_entry *));
+    return dict;
+}
+
+struct hash_table_entry *hash_table_lookup(struct hash_table *dict, int key)
+{
+    int index = hash(key, dict->key_size);
+    struct hash_table_entry *next_value = dict->entries[index];
+    int depth = 0;
+    while (next_value != NULL)
+    {
+        if (next_value->key == key)
+        {
+            return next_value;
+        }
+        next_value = next_value->next;
+        depth++;
+    }
+    return NULL;
+}
+
+void hash_table_insert(struct hash_table *dict, int key, int value)
+{
+    struct hash_table_entry *existing_entry = hash_table_lookup(dict, key);
+    if (existing_entry)
+    {
+        existing_entry->value = value;
+    }
+    else
+    {
+        int index = hash(key, dict->key_size) % dict->size;
+        existing_entry = dict->entries[index];
+        struct hash_table_entry *new_entry = malloc(sizeof(struct hash_table_entry));
+        new_entry->key = key;
+        new_entry->value = value;
+        new_entry->next = existing_entry;
+        dict->entries[index] = new_entry;
+    }
+}
+
+bool hash_table_contains(struct hash_table *dict, int key)
+{
+    return hash_table_lookup(dict, key) != NULL;
+}
